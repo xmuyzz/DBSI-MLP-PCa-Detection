58bb216c2537a932f61a78c8c57117529e47a9be
xmuyzz <xmuyzz@gmail.com>
2022-04-10 21:06:21 -0400

    first commit

--- statistics/get_prc.py
+++ statistics/get_prc.py
@@ -1,14 +1,15 @@
 import os
 import numpy as np
 import pandas as pd
-from utils.plot_roc import plot_roc
+from utils.plot_prc import plot_prc
 from utils.roc_bootstrap import roc_bootstrap
 
 
-def get_roc(proj_dir, output_dir, bootstrap, data_type):
+
+def get_prc(proj_dir, output_dir, data_type):
     
     pro_data_dir = os.path.join(proj_dir, 'pro_data')
-    
+
     if data_type == 'invivo':
         fn1 = 'invivo_voxel_pred.csv'
         fn2 = 'invivo_pat_pred.csv'
@@ -17,12 +18,12 @@ def get_roc(proj_dir, output_dir, bootstrap, data_type):
         fn2 = 'exvivo_pat_pred.csv'
 
     # voxel level data
-    df1 = pd.read_csv(os.path.join(pro_data_dir, fn1))
+    df1 = pd.read_csv(os.path.join(pro_data_dir, 'test_voxel_pred.csv'))
     y_test_vox = df1['y_test'].to_numpy()
     y_pred_vox = df1['y_pred'].to_numpy()
     
     # patient level data
-    df2 = pd.read_csv(os.path.join(pro_data_dir, fn2))
+    df2 = pd.read_csv(os.path.join(pro_data_dir, 'test_pat_pred.csv'))
     y_test_pat = df2['y_test'].to_numpy()
     y_pred_pat = df2['y_pred'].to_numpy()
 
@@ -33,21 +34,15 @@ def get_roc(proj_dir, output_dir, bootstrap, data_type):
     
     for pred, label, level in zip(preds, labels, level):
         
-        auc = plot_roc(
+        prc_auc = plot_prc(
             save_dir=output_dir, 
             y_true=label, 
             y_pred=pred, 
             level=level, 
-            color='blue',
+            color='red',
             data_type=data_type
             )
-        print(level, auc)
     
-        ### calculate roc, tpr, tnr with 1000 bootstrap
-        roc_stat = roc_bootstrap(
-            bootstrap=bootstrap,
-            y_true=label,
-            y_pred=pred
-            )
+        print(level, prc_auc)
 
-    return roc_stat
+    return prc_auc
--- statistics/get_roc.py
+++ statistics/get_roc.py
@@ -1,14 +1,15 @@
 import os
 import numpy as np
 import pandas as pd
-from utils.plot_prc import plot_prc
+from utils.plot_roc import plot_roc
 from utils.roc_bootstrap import roc_bootstrap
 
 
-def get_prc(proj_dir, output_dir, data_type):
+
+def get_roc(proj_dir, output_dir, bootstrap, data_type):
     
     pro_data_dir = os.path.join(proj_dir, 'pro_data')
-
+    
     if data_type == 'invivo':
         fn1 = 'invivo_voxel_pred.csv'
         fn2 = 'invivo_pat_pred.csv'
@@ -17,12 +18,12 @@ def get_prc(proj_dir, output_dir, data_type):
         fn2 = 'exvivo_pat_pred.csv'
 
     # voxel level data
-    df1 = pd.read_csv(os.path.join(pro_data_dir, 'test_voxel_pred.csv'))
+    df1 = pd.read_csv(os.path.join(pro_data_dir, fn1))
     y_test_vox = df1['y_test'].to_numpy()
     y_pred_vox = df1['y_pred'].to_numpy()
     
     # patient level data
-    df2 = pd.read_csv(os.path.join(pro_data_dir, 'test_pat_pred.csv'))
+    df2 = pd.read_csv(os.path.join(pro_data_dir, fn2))
     y_test_pat = df2['y_test'].to_numpy()
     y_pred_pat = df2['y_pred'].to_numpy()
 
@@ -33,15 +34,21 @@ def get_prc(proj_dir, output_dir, data_type):
     
     for pred, label, level in zip(preds, labels, level):
         
-        prc_auc = plot_prc(
+        auc = plot_roc(
             save_dir=output_dir, 
             y_true=label, 
             y_pred=pred, 
             level=level, 
-            color='red',
+            color='blue',
             data_type=data_type
             )
+        print(level, auc)
     
-        print(level, prc_auc)
+        ### calculate roc, tpr, tnr with 1000 bootstrap
+        roc_stat = roc_bootstrap(
+            bootstrap=bootstrap,
+            y_true=label,
+            y_pred=pred
+            )
 
-    return prc_auc
+    return roc_stat
